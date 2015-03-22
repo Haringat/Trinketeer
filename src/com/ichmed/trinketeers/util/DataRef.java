a3e028ba6a90ad3f16ae6df1898fd070db341c40
Marcel Mundl <Marcel@Mundlhome.de>
2015-03-23 00:31:10 +0100

    made fields final

--- src/com/ichmed/trinketeers/util/DataRef.java
+++ src/com/ichmed/trinketeers/util/DataRef.java
@@ -7,6 +7,6 @@ public class DataRef
 
 	public static final File elementsFile = new File("resc/data/elements.json");
 	public static final File entitiesFile = new File("resc/data/entities.json");
-	public static File defaultLibrary = new File("resc/textures/defaultLibrary.png");
-	public static File defaultLibraryRaw = new File("resc/textures/raw/defaultLibrary");
+	public static final File defaultLibrary = new File("resc/textures/defaultLibrary.png");
+	public static final File defaultLibraryRaw = new File("resc/textures/raw/defaultLibrary");
 }
